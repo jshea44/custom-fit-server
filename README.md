93809bffcc9cae5df95adc7932960ca4557705eb
joshuashea <joshshea44@gmail.com>
2023-12-12 12:29:15 -0800

    added exercise data model

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# custom-fit-server
+# Custom Fit Server
+
+> Server to the Custom Fit application.
+
+## Setup
+
+## Contributors
+
+- Joshua Shea
+
+## Resources
+
+- ChatGPT
---
+++ src/models/ExerciseModel.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const ExerciseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  sets: {
+    type: Number,
+    required: false,
+  },
+  reps: {
+    type: Number,
+    required: false,
+  },
+  weight: {
+    type: Number,
+    required: false,
+  },
+  time: {
+    type: Number,
+    required: false,
+  },
+  distance: {
+    type: Number,
+    required: false,
+  },
+  isSuperset: {
+    type: Boolean,
+    default: false,
+  },
+  isDropset: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const ExerciseModel = mongoose.model('Exercise', ExerciseSchema);
+
+module.exports = ExerciseModel;
