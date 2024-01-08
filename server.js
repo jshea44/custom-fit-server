c230ff105597bcfddc95ac04c1ada9513d3eadd3
joshuashea <joshshea44@gmail.com>
2024-01-08 15:32:20 -0800

    connecting to mongoDB as well as ability to create exercises now working

--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exerciseRouter = require('./src/routes/exercise.js');
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
