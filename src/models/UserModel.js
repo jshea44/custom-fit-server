0570b8f807dfcb3a7328846c7b2430ccbc0da3fb
joshuashea <joshshea44@gmail.com>
2024-01-16 14:51:37 -0800

    fixed syntax errors with user model as well as specified collection for database

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,14 +1,19 @@
 'use strict';
 
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
+const WorkoutModel = require('./WorkoutModel.js');
 
-const UserSchema = new mongoose.UserSchema({
-  username: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    workouts: [WorkoutModel.schema],
   },
-});
+  { collection: 'users' }
+);
 
-const UserModel = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model('User', UserSchema, 'users');
 
 module.exports = UserModel;
